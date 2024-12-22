3a73dd9529509fdcad793b926ced4d336ff00505
Bjolek <141151371+Bjolek@users.noreply.github.com>
2024-12-22 18:29:11 +0200

    shop2

--- RunServer.py
+++ RunServer.py
@@ -5,7 +5,19 @@ app = Flask("Shop")
 db_name = "Site.db"
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def base():
+    return render_template("base.html")
+
+@app.route("/about_us")
+def about_us():
+    return render_template("about_us.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/products")
+def products():
+    return render_template("products.html")
 
 app.run()
